5f3ef216ab25bd700d76b2b9349a4aad29a1561c
mpanayotov <mihail.workbuz@gmail.com>
2016-10-26 13:18:50 +0300

    chat-project changes

--- NodeProjects/chat-project/index.js
+++ NodeProjects/chat-project/index.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
+//connect
 io.on('connection', function(socket){
   socket.broadcast.emit('hi', "hello");
   console.log('a user connected');
