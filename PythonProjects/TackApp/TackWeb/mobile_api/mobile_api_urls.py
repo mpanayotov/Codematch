1ccec4039ac07f78835adad010e9fd7c51940b9c
mpanayotov <mihail.workbuz@gmail.com>
2016-10-26 16:22:12 +0300

    fixes

--- PythonProjects/TackApp/TackWeb/mobile_api/mobile_api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-moible_api_patterns = patterns('',
-    url(r'^getStickers/$', 'mobile_api.views.get_stickers'),
-    #url(r'^getAvailabilityAndDisplayPartners/$', 'mobile_api.views.get_parking_information_and_show_partners_around'),
-)
