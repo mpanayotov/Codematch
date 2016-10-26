1da9a7849025cacb315b88e8f4945a6885a258a4
mpanayotov <mihail.workbuz@gmail.com>
2016-10-26 16:49:05 +0300

    fixes

--- TackWeb/homepage/views.py
+++
@@ -1,5 +0,0 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-def render_landing_page(request):
-    return render_to_response('index.html', {}, context_instance=RequestContext(request))
